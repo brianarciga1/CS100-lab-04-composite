592c7f0332f9acae1ab921bc7431f8d4d3a05c0f
Brian Arciga <barci003@ucr.edu>
2020-11-04 03:42:35 -0800

    Updated Rand class and created test

--- op_test.hpp
+++ op_test.hpp
@@ -4,10 +4,21 @@
 #include "gtest/gtest.h"
 
 #include "op.hpp"
+#include "rand.hpp"
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
     EXPECT_EQ(test->evaluate(), 8);
+}
+
+TEST(OpTest, OpStringifyNonZero) {
+    Op* test = new Op(8);
+    EXPECT_EQ(test->stringify(), "8.000000");
+}
+
+TEST(OpTest, EvalRand) {
+    Rand* test = new Rand();
+    EXPECT_NEAR(100, test->evaluate(), 0);
 }
 
 #endif //__OP_TEST_HPP__
